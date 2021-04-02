3735f56e23feac9358acdb982b44083eac000e6b
Irene Hodge <irenehodge90@gmail.com>
2021-04-01 17:21:09 -0700

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./client/App.jsx",
+  output: {
+    path: path.resolve(__dirname, "client"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+};
